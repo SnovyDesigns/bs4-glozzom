a304fbf8144e7d938684439301ced0e9d0c4eea0
Krzysztof Piekarz <krzysztofpiekarz@iMac-Krzysztof.local>
2018-09-01 22:32:25 +0200

    Services Page With FAQ Accordion

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './scss/main.scss';
 import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/collapse';
 import './js/ekko-lightbox';
 import './js/slick';
 import './js/scripts';
